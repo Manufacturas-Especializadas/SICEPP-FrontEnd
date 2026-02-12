94c86c275f02cf2e0a9971f21e5123c0f83fae8d
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-02-12 15:43:14 -0600

    Formulario de registro de EPP

--- src/pages/EPP/EppIndex.tsx
+++ src/pages/EPP/EppIndex.tsx
@@ -1,3 +1,151 @@
+import { HardHat } from "lucide-react";
+import { FormCard } from "../../components/FormCard/FormCard";
+import { Input } from "../../components/CustomInputs/Input";
+import { useEppTypes } from "../../hooks/useEppTypes";
+import { useEppForm } from "../../hooks/useEppForm";
+import { FloatingSelect } from "../../components/CustomInputs/FloatingSelect";
+import { useSizes } from "../../hooks/useSizes";
+import { useReasonRequest } from "../../hooks/useReasonRequest";
+import { usePreviousCondition } from "../../hooks/usePreviousCondition";
+import { LoadingSkeleton } from "../../components/LoadingSkeleton/LoadingSkeleton";
+
 export const EppIndex = () => {
-  return <div>EppIndex</div>;
+  const { formData, loading, handleChange, handleSubmit } = useEppForm();
+
+  const { data: eppTypes } = useEppTypes();
+  const { data: sizes } = useSizes();
+  const { data: reasonRequest } = useReasonRequest();
+  const { data: previousCondition } = usePreviousCondition();
+
+  const eppTypesOptions =
+    eppTypes.map((e) => ({
+      label: e.nameType,
+      value: e.id,
+    })) || [];
+
+  const sizesOptions =
+    sizes.map((e) => ({
+      label: e.nameSize,
+      value: e.id,
+    })) || [];
+
+  const reasonRequestOptions =
+    reasonRequest.map((e) => ({
+      label: e.nameReason,
+      value: e.id,
+    })) || [];
+
+  const previousConditionOptions =
+    previousCondition.map((e) => ({
+      label: e.nameCondition,
+      value: e.id,
+    })) || [];
+
+  if (loading) return <LoadingSkeleton />;
+
+  return (
+    <div className="min-h-screen bg-slate-100 p-4 md:p-8">
+      <FormCard
+        title="REGISTRO DE EPP"
+        subtitle="Control de equipo de protección persona para colaboradores"
+        icon={<HardHat size={24} />}
+        onSubmit={handleSubmit}
+      >
+        <div className="flex flex-col gap-1.5">
+          <Input
+            label="Nombre"
+            value={formData.name}
+            onChange={(e) => {
+              handleChange("name", e.target.value);
+            }}
+          />
+        </div>
+
+        <div className="flex flex-col gap-1.5">
+          <Input
+            label="Área"
+            value={formData.area}
+            onChange={(e) => {
+              handleChange("area", e.target.value);
+            }}
+          />
+        </div>
+
+        <div className="flex flex-col gap-1.5">
+          <Input
+            label="Puesto"
+            value={formData.position}
+            onChange={(e) => {
+              handleChange("position", e.target.value);
+            }}
+          />
+        </div>
+
+        <div className="flex flex-col gap-1.5">
+          <Input
+            label="Turno"
+            value={formData.shift}
+            onChange={(e) => {
+              handleChange("shift", e.target.value);
+            }}
+          />
+        </div>
+
+        <div className="flex flex-col gap-1.5">
+          <FloatingSelect
+            label="Tipo de EPP"
+            options={eppTypesOptions}
+            value={formData.eppTypeId}
+            onChange={(value) => {
+              handleChange("eppTypeId", Number(value));
+            }}
+          />
+        </div>
+
+        <div className="flex flex-col gap-1.5">
+          <FloatingSelect
+            label="Talla (si aplica)"
+            options={sizesOptions}
+            value={formData.sizeId}
+            onChange={(value) => {
+              handleChange("sizeId", Number(value));
+            }}
+          />
+        </div>
+
+        <div className="flex flex-col gap-1.5">
+          <Input
+            label="Cantidad solicitada"
+            type="number"
+            value={formData.requestedQuantity || ""}
+            onChange={(e) => {
+              handleChange("requestedQuantity", e.target.value);
+            }}
+          />
+        </div>
+
+        <div className="flex flex-col gap-1.5">
+          <FloatingSelect
+            label="Motivo de la solicitud"
+            options={reasonRequestOptions}
+            value={formData.reasonRequestId}
+            onChange={(value) => {
+              handleChange("reasonRequestId", Number(value));
+            }}
+          />
+        </div>
+
+        <div className="flex flex-col gap-1.5">
+          <FloatingSelect
+            label="Condición del EPP anterior"
+            options={previousConditionOptions}
+            value={formData.previousConditionId}
+            onChange={(value) => {
+              handleChange("previousConditionId", Number(value));
+            }}
+          />
+        </div>
+      </FormCard>
+    </div>
+  );
 };
