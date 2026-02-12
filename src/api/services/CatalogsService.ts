be26cb3a536b73b1391830ed9ecc179079612da2
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-02-12 14:17:29 -0600

    Metodo para obtener los motivos de la solicitud

--- src/api/services/CatalogsService.ts
+++ src/api/services/CatalogsService.ts
@@ -1,10 +1,12 @@
 import { API_CONFIG } from "../../config/api";
-import type { EppTypes, Sizes } from "../../types/types";
+import type { EppTypes, ReasonRequest, Sizes } from "../../types/types";
 import { apiClient } from "../client";
 
 class CatalogsService {
   private getEppTypesEndpoint = API_CONFIG.endpoints.catalogs.getEppTypes;
   private getSizesEndpoint = API_CONFIG.endpoints.catalogs.getSizes;
+  private getReasonRequestEndpoint =
+    API_CONFIG.endpoints.catalogs.getReasonRequest;
 
   async getEppTypes(): Promise<EppTypes[]> {
     return apiClient.get<EppTypes[]>(this.getEppTypesEndpoint);
@@ -12,6 +14,10 @@ class CatalogsService {
 
   async getSizes(): Promise<Sizes[]> {
     return apiClient.get<Sizes[]>(this.getSizesEndpoint);
+  }
+
+  async getReasonRequest(): Promise<ReasonRequest[]> {
+    return apiClient.get<ReasonRequest[]>(this.getReasonRequestEndpoint);
   }
 }
 
