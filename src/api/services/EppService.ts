133e07b54797e77a0d6da1bb54d6fe0c294dcae3
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-02-13 08:34:05 -0600

    Metodo para registrar un EPP

--- src/api/services/EppService.ts
+++ src/api/services/EppService.ts
@@ -1,3 +1,13 @@
-class EppService {}
+import { API_CONFIG } from "../../config/api";
+import type { Epp } from "../../types/types";
+import { apiClient } from "../client";
+
+class EppService {
+  private createEndpoint = API_CONFIG.endpoints.epp.create;
+
+  async create(formData: Epp): Promise<Epp> {
+    return apiClient.post<Epp>(this.createEndpoint, formData);
+  }
+}
 
 export const eppService = new EppService();
