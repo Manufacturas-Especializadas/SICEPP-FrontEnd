139d5af9cf3edd72177574bad4be80ea897cfa0d
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-02-17 11:14:15 -0600

    feat: ruta para reportes

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import { EppIndex } from "../pages/EPP/EppIndex";
 import { Store } from "../pages/Store/Store";
+import { MonthlyReports } from "../pages/MonthlyReports/MonthlyReports";
 
 export const MyRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<EppIndex />} />
       <Route path="/almacen" element={<Store />} />
+      <Route path="/reportes" element={<MonthlyReports />} />
     </Routes>
   );
 };
