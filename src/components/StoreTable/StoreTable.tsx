ce72dcbbd604d9f020cbe885dde294feb884c66a
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-02-17 07:42:49 -0600

    feat: sistema de paginacion

--- src/components/StoreTable/StoreTable.tsx
+++ src/components/StoreTable/StoreTable.tsx
@@ -1,4 +1,4 @@
-import { useState, type JSX } from "react";
+import { useMemo, useState, type JSX } from "react";
 import type { EppListItem, StatusType } from "../../types/types";
 import { formatDateTime } from "../../utils/dateFormatter";
 import { StoreDetailModal } from "../StoreDetailModal/StoreDetailModal";
@@ -7,9 +7,20 @@ interface Props {
   data: EppListItem[];
 }
 
+const ITEMS_PER_PAGE = 10;
+
 export const StoreTable = ({ data }: Props): JSX.Element => {
   const [selectedId, setSelectedId] = useState<number | null>(null);
   const [open, setOpen] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const totalPages = Math.ceil(data.length / ITEMS_PER_PAGE);
+
+  const paginatedData = useMemo(() => {
+    const start = (currentPage - 1) * ITEMS_PER_PAGE;
+
+    return data.slice(start, start + ITEMS_PER_PAGE);
+  }, [data, currentPage]);
 
   return (
     <div className="bg-white rounded-2xl shadow-sm overflow-hidden">
@@ -29,7 +40,7 @@ export const StoreTable = ({ data }: Props): JSX.Element => {
           </thead>
 
           <tbody className="divide-y">
-            {data.map((item) => (
+            {paginatedData.map((item) => (
               <tr key={item.id} className="hover:bg-gray-50 transition">
                 <td className="px-6 py-4 font-medium">{item.name}</td>
                 <td className="px-6 py-4">{item.area}</td>
@@ -58,6 +69,49 @@ export const StoreTable = ({ data }: Props): JSX.Element => {
             ))}
           </tbody>
         </table>
+      </div>
+      <div className="flex justify-between items-center px-6 py-4 border-t bg-gray-50 text-sm">
+        <span className="text-gray-500">
+          Página {currentPage} de {totalPages}
+        </span>
+
+        <div className="flex gap-2">
+          <button
+            disabled={currentPage === 1}
+            onClick={() => setCurrentPage((prev) => prev - 1)}
+            className="px-3 py-1 rounded-md border text-gray-600 
+            disabled:opacity-40 hover:cursor-pointer"
+          >
+            ←
+          </button>
+
+          {Array.from({ length: totalPages }).map((_, index) => {
+            const page = index + 1;
+
+            return (
+              <button
+                key={page}
+                onClick={() => setCurrentPage(page)}
+                className={`px-3 py-1 rounded-md border ${
+                  currentPage === page
+                    ? "bg-blue-600 text-white border-blue-600"
+                    : "text-gray-600"
+                }`}
+              >
+                {page}
+              </button>
+            );
+          })}
+
+          <button
+            disabled={currentPage === totalPages}
+            onClick={() => setCurrentPage((prev) => prev + 1)}
+            className="px-3 py-1 rounded-md border text-gray-600 
+            disabled:opacity-40 hover:cursor-pointer"
+          >
+            →
+          </button>
+        </div>
       </div>
       <StoreDetailModal
         eppId={selectedId}
