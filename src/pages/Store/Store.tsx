abeb8b6d81d2a7053730b9d0a8c653311c009519
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-02-17 07:48:26 -0600

    feat: boton de navegacion hacia reportes

--- src/pages/Store/Store.tsx
+++ src/pages/Store/Store.tsx
@@ -1,9 +1,12 @@
+import { useNavigate } from "react-router-dom";
 import { StoreStatusCard } from "../../components/StoreStatus/StoreStatus";
 import { StoreTable } from "../../components/StoreTable/StoreTable";
 import { useEpp } from "../../hooks/useEpp";
 
 export const Store = () => {
   const { data } = useEpp();
+
+  const navigate = useNavigate();
 
   return (
     <div className="min-h-screen bg-gray-100 p-6 space-y-6">
@@ -15,6 +18,16 @@ export const Store = () => {
       </header>
 
       <StoreStatusCard data={data} />
+
+      <div className="flex justify-end">
+        <button
+          onClick={() => navigate("/reportes")}
+          className="flex items-center px-5 py-2 rounded-xl bg-green-600 text-white
+          hover:bg-green-700 transition shadow-sm hover:cursor-pointer"
+        >
+          Reportes
+        </button>
+      </div>
       <StoreTable data={data} />
     </div>
   );
