82e51f7a301620da76670466b422801c75db0cfa
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-02-18 08:11:47 -0600

    feat: toast global

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,45 @@
 import { BrowserRouter } from "react-router-dom";
 import { Navbar } from "./components/Navbar/Navbar";
 import { MyRoutes } from "./routes/Routes";
+import { Toaster } from "react-hot-toast";
 
 export const App = () => {
   return (
     <BrowserRouter>
+      <Toaster
+        toastOptions={{
+          className: "",
+          style: {
+            background: "#363636",
+            color: "#fff",
+            zIndex: 9999,
+          },
+          success: {
+            duration: 10000,
+            position: "top-right",
+            style: {
+              background: "#10B981",
+              color: "#fff",
+            },
+          },
+          error: {
+            duration: 5000,
+            position: "top-right",
+            style: {
+              background: "#EF4444",
+              color: "#fff",
+            },
+          },
+          loading: {
+            duration: Infinity,
+            position: "top-right",
+            style: {
+              background: "#3B82F6",
+              color: "#fff",
+            },
+          },
+        }}
+      />
       <Navbar />
       <main>
         <MyRoutes />
